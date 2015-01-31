d33c66493b829acaf0e378b939165062fd90374d
Ali Mohamed <ali.ahmmed.mohamed@gmail.com>
2015-01-31 20:42:25 +0200

    Update before_restart.rb

--- deploy/before_restart.rb
+++ deploy/before_restart.rb
@@ -1,7 +1,6 @@
 node[:deploy].each do |application, deploy_item|
 
-  puts application.inspect.to_s
-  puts deploy_item.inspect.to_s
+  puts deploy_item[:rails_env].inspect.to_s
 	execute "Lib folder shortcut" do
 	  command "ls"
 	  action :run
