cb74bee881404ad84d4aa95025477c6dae24bcb6
Ali Mohamed <ali.ahmmed.mohamed@gmail.com>
2015-02-03 16:21:47 +0200

    Update before_restart.rb

--- deploy/before_restart.rb
+++ deploy/before_restart.rb
@@ -1,7 +1,11 @@
 node[:deploy].each do |application, deploy_item|
 
-  puts deploy_item[:rails_env].inspect.to_s
+  if deploy_item[:domains].map{|d| d.include? 'tayaservice.com'}.include? true
+    puts deploy_item[:rails_env].inspect.to_s
+    ENV['CDN_YAHKI2'] = 'cdn2.yahki.com'
+  end
 	execute "Lib folder shortcut" do
+	  environment 'CDN_YAHKI' => "cdn.yahki.com"
 	  command "ls"
 	  action :run
 	end
