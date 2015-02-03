722ec9157c0ff071d6461a25ff80417497906d87
Ali Mohamed <ali.ahmmed.mohamed@gmail.com>
2015-02-03 17:09:02 +0200

    Update before_restart.rb

--- deploy/before_restart.rb
+++ deploy/before_restart.rb
@@ -1,6 +1,6 @@
 node[:deploy].each do |application, deploy_item|
 
-  if deploy_item[:domains].map{|d| d.include? 'tayaservice.com'}.include? true
+  if deploy_item[:domains] && deploy_item[:domains].map{|d| d.include? 'tayaservice.com'}.include? true
     puts deploy_item[:rails_env].inspect.to_s
     ENV['CDN_YAHKI2'] = 'cdn2.yahki.com'
   end
